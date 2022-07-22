528832049d60981fd59d701f2443858392868228
Daniel Harte <norgeous@users.noreply.github.com>
2022-07-22 20:11:21 +0100

    Update entry.js

--- entry.js
+++ entry.js
@@ -3,19 +3,14 @@
   './src/tilesets/ascii-squares.js',
   './src/tilesets/ascii-triangles.js',
   './src/tilesets/index.js',
-
   './src/utils.js',
-
   './src/classes/GridData.js',
   './src/hooks/useGrid.js',
-
   './src/components/styled.js',
-
   './src/components/Cell.js',
   './src/components/Grid.js',
   './src/components/App.js',
-
-  './src/index.js', // important: this should be last in the list
+  './src/index.js',
 ].forEach(url => {
   const tag = `<script type="text/babel" data-type="module" data-plugins="transform-modules-umd" data-presets="react,stage-3" src="${url}" />`;
   document.head.insertAdjacentHTML('beforeend', tag);
