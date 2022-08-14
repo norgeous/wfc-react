40fe81c2d543ba84beb5d06e8f41bcef5e402a45
Daniel Harte <norgeous@users.noreply.github.com>
2022-08-14 14:31:42 +0100

    Update GridDisplay.js

--- src/components/GridDisplay.js
+++ src/components/GridDisplay.js
@@ -17,44 +17,44 @@ const GridDisplay = ({
 
   return (
     <TileGrid size={size} width={width} height={height} style={style}>
-        {tileGrid.map(({ x, y, tileValue, solveLevel, valid }) => {
-            const tabIndexOffset = 1;
-            const tabIndex = tabIndexOffset + x + ((y+1) * (width+1)) + (y*width);
-            return (
-              <Tile
-                key={`${x}:${y}`}
-                tabIndex={tabIndex}
-                valid={valid}
-                onClick={() => collapse4(x, y)}
-              >
-                <TileFace
-                  tileId={tileValue}
-                  solveLevel={solveLevel}
-                  size={size}
-                />
-              </Tile>
-            );
-          })}
+      {tileGrid.map(({ x, y, tileValue, solveLevel, valid }) => {
+        const tabIndexOffset = 1;
+        const tabIndex = tabIndexOffset + x + ((y+1) * (width+1)) + (y*width);
+        return (
+          <Tile
+            key={`${x}:${y}`}
+            tabIndex={tabIndex}
+            valid={valid}
+            onClick={() => collapse4(x, y)}
+          >
+            <TileFace
+              tileId={tileValue}
+              solveLevel={solveLevel}
+              size={size}
+            />
+          </Tile>
+        );
+      })}
 
-        <PointGrid size={size} width={width} height={height}>
-          {grid.map(({ x, y, v }) => {
-            const tabIndexOffset = 1;
-            const tabIndex = tabIndexOffset + x + (width+1)*y + (y*width);
-            return (
-              <div key={`${x}:${y}`}>
-                <Point
-                  title={`${x}:${y}`}
-                  tabIndex={tabIndex}
-                  debug={debug}
-                  size={size}
-                  onClick={() => collapseSingle(x, y)}
-                >
-                  {v === '*' ? '✸' : v}
-                </Point>
-              </div>
-            );
-          })}
-        </PointGrid>
+      <PointGrid size={size} width={width} height={height}>
+        {grid.map(({ x, y, v }) => {
+          const tabIndexOffset = 1;
+          const tabIndex = tabIndexOffset + x + (width+1)*y + (y*width);
+          return (
+            <div key={`${x}:${y}`}>
+              <Point
+                title={`${x}:${y}`}
+                tabIndex={tabIndex}
+                debug={debug}
+                size={size}
+                onClick={() => collapseSingle(x, y)}
+              >
+                {v === '*' ? '✸' : v}
+              </Point>
+            </div>
+          );
+        })}
+      </PointGrid>
     </TileGrid>
   );
 };
