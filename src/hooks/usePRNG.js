bf6557345587652cf30bda43cba08683d67b5aa8
Daniel Harte <norgeous@users.noreply.github.com>
2022-08-18 09:44:42 +0100

    prng step counter

--- src/hooks/usePRNG.js
+++ src/hooks/usePRNG.js
@@ -9,18 +9,24 @@ const sanitise = (seed) => {
 };
 
 const usePRNG = (seed = 0) => {
+  const [prngStepCount, setPrngStepCount] = useState(0);
   const [prn, setPrn] = useState();
   const prf = (prn - 1) / (mersenne8 - 1);
   
-  const nextPrn = () => setPrn(prn => prn * cyv % mersenne8);
+  const nextPrn = () => {
+    setPrn(prn => prn * cyv % mersenne8);
+    setPrngStepCount(s => s + 1);
+  };
   const resetPrng = () => {
     setPrn(sanitise(seed));
+    setPrngStepCount(0);
     nextPrn();
   };
 
   useEffect(resetPrng, [seed]);
 
   return {
+    prngStepCount,
     prn,
     prf,
     nextPrn,
