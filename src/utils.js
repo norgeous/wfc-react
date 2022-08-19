8d3583d2bd78541bd9c60ef312accbaf010c468a
norgeous <daniel@harte.me>
2022-08-19 19:48:31 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,3 @@
-// // arrays are objects too!
-// export const randomFrom = (obj = {}, random) => {
-//   const keys = Object.keys(obj);
-//   return obj[keys[keys.length * random << 0]];
-// };
-
-// export const weightedRandomFrom = (obj = {}, weights = []) => {
-//   const keys = Object.keys(obj);
-//   const totalWeight = weights.reduce((acc, weight) => acc + weight, 0);
-//   const r = Math.random() * totalWeight;
-//   let i, sum = 0;
-//   for (i in keys) {
-//     sum += weights[i];
-//     if (r <= sum) return obj[i];
-//   }
-// };
-
 const arrayRotate = (arr) => {
   arr.unshift(arr.pop());
   return arr;
@@ -33,32 +16,5 @@ export const rotate4 = (arr) => {
 };
 
 export const unique = (value, index, self) => self.indexOf(value) === index;
+
 export const byXY = (x, y) => (cell) => cell.x === x && cell.y === y;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export function PRNG(seed = 0) {
-  this._seed = seed % 2147483647;
-  if (this._seed <= 0) this._seed += 2147483646;
-}
-PRNG.prototype.next = function () {
-  return this._seed = this._seed * 16807 % 2147483647;
-};
-// We know that result of next() will be 1 to 2147483646 (inclusive).
-PRNG.prototype.nextFloat = function () {
-  return (this.next() - 1) / 2147483646;
-};
