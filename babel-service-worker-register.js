de611a59cfd4f7c6937530c0da932a3dc94e29ff
Daniel Harte <norgeous@users.noreply.github.com>
2022-08-20 13:40:06 +0100

    Update babel-service-worker-register.js

--- babel-service-worker-register.js
+++ babel-service-worker-register.js
@@ -1,3 +1,5 @@
+const { pathname } = window.location
+
 const load = (uri) => {
   const tag = `<script type="module" src="${uri}" />`;
   document.body.insertAdjacentHTML('beforeend', tag);
@@ -6,7 +8,7 @@ const load = (uri) => {
 const loadIndex = () => load('./src/index');
 
 (async () => {
-  await navigator.serviceWorker.register('/wfc-react/babel-service-worker.js', { scope: '/wfc-react/' });
+  await navigator.serviceWorker.register(`${pathname}babel-service-worker.js`, { scope: pathname });
   await navigator.serviceWorker.ready;
   if (!navigator.serviceWorker.controller) {
     window.location.reload(); // https://stackoverflow.com/a/62596701
